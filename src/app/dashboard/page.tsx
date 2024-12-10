916da0116a82b2e8fcca58418011e145ebc36642
Cameron King <35708477+cameronking4@users.noreply.github.com>
2024-12-10 02:56:52 -0500

    update Typescript + UX

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -234,6 +234,21 @@ export default function Dashboard() {
 
   const convertedItems = useMemo(() => convertItemsToAccounts(items, accounts_info), [items, accounts_info]);
 
+  const spinner = (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      xmlns="http://www.w3.org/2000/svg"
+      className="size-5 animate-spin stroke-zinc-400"
+    >
+      <path d="M12 3v3m6.366-.366-2.12 2.12M21 12h-3m.366 6.366-2.12-2.12M12 21v-3m-6.366.366 2.12-2.12M3 12h3m-.366-6.366 2.12 2.12"></path>
+    </svg>
+  );
+
   return (
     <>
     { convertedItems?.length > 0 ? 
@@ -322,18 +337,3 @@ export default function Dashboard() {
     </>
   );
 }
-
-export const spinner = (
-  <svg
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="1.5"
-    viewBox="0 0 24 24"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    xmlns="http://www.w3.org/2000/svg"
-    className="size-5 animate-spin stroke-zinc-400"
-  >
-    <path d="M12 3v3m6.366-.366-2.12 2.12M21 12h-3m.366 6.366-2.12-2.12M12 21v-3m-6.366.366 2.12-2.12M3 12h3m-.366-6.366 2.12 2.12"></path>
-  </svg>
-)
